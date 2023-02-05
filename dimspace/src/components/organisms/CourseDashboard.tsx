37797b5ad348c9d8926841f9a3148d91c6f6caeb
Eduardo Szeckir <eszeckirjr@gmail.com>
2023-02-04 20:52:06 -0800

    add small error message for blank screen

--- dimspace/src/components/organisms/CourseDashboard.tsx
+++ dimspace/src/components/organisms/CourseDashboard.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, HStack, Icon, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, HStack, Icon, Text } from "@chakra-ui/react";
 import { sampleCourses } from "../../utils/sampleData";
 import CourseCard from "../molecules/CourseCard";
 import { BsEye, BsEyeSlash } from "react-icons/bs";
@@ -7,6 +7,11 @@ import { Term } from "../../utils/types";
 
 const CourseDashboard = ({ term }: { term: Term }) => {
   const [displayCompleted, setDisplayCompleted] = useState(true);
+
+  const filteredCourses = sampleCourses
+    .filter((course) => course.term === term || term === "All")
+    .filter((course) => !course.completed || displayCompleted);
+
   return (
     <Box p={4}>
       <HStack justify="end">
@@ -33,15 +38,11 @@ const CourseDashboard = ({ term }: { term: Term }) => {
         mx="auto"
         maxW={["100%", "90%", "70%"]}
       >
-        {sampleCourses.map((course) => {
-          return course.completed
-            ? displayCompleted && (course.term === term || term === "All") && (
-                <CourseCard course={course} />
-              )
-            : (course.term === term || term === "All") && (
-                <CourseCard course={course} />
-              );
-        })}
+        {filteredCourses.length > 0 ? (
+          filteredCourses.map((course) => <CourseCard course={course} />)
+        ) : (
+          <Heading fontWeight="medium">Uh oh... no courses found</Heading>
+        )}
       </Flex>
     </Box>
   );
