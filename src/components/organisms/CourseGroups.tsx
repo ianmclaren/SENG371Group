979edb1c619efcd1895047d7a1cfd007ad68fe03
Shake1999 <northstarsh@gmail.com>
2023-03-02 23:32:36 -0800

    Basics of course pages and course Home

---
+++ src/components/organisms/CourseGroups.tsx
@@ -0,0 +1,22 @@
+import { Box, HStack} from "@chakra-ui/react";
+import { useSearchParams } from "react-router-dom";
+import CourseHeader from "../common/CourseHeader";
+import WeeksColumn from "../common/WeeksColumn";
+
+const CourseGroups = () => {
+    const [queryParameters] = useSearchParams();
+    var id = queryParameters.get("courseID");
+    const courseID = id == null ? '': id; // edit if you know a more graceful way to handle the null
+
+    return (
+      <Box>
+        <CourseHeader courseID={courseID} />
+        <Box w="100%" h={0.5} bgColor="white" />
+        <HStack>
+          <WeeksColumn courseID={courseID}/>
+        </HStack>
+      </Box>
+    );
+  };
+
+  export default CourseGroups;
