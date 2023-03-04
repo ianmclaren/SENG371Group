207e3450bcf23ff241f38663157fd704182988da
Rebecca Marshall <rebeccamarshall@uvic.ca>
2023-03-04 12:19:25 -0800

    loop through weeksColumn component

--- src/components/common/WeeksColumn.tsx
+++ src/components/common/WeeksColumn.tsx
@@ -1,17 +1,25 @@
 import { Box, Text, VStack, Flex} from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
-// if anyone can loop this. Thanks!
-
 const WeeksColumn = ({courseID}: {courseID: string;}) => {
     const navigate = useNavigate();
-    const pathname =window.location.hash.split('?')[0].slice(1,);
+    const pathname = window.location.hash.split('?')[0].slice(1,);
+
+    const weeks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+    const handleWeekClick = (week: number) => {
+        navigate({
+            pathname: pathname,
+            search: `?courseID=${courseID}&week=${week}`,
+        });
+    };
+
     return (
-          <VStack>
+        <VStack>
             <Box paddingTop={4} paddingLeft={5}>
                 <Text fontWeight="medium">Week</Text>
             </Box>
-              <Flex
+            <Flex
                 gap={2}
                 p={3}
                 flexDirection="column"
@@ -19,130 +27,25 @@ const WeeksColumn = ({courseID}: {courseID: string;}) => {
                 align="stretch"
                 mx="auto"
                 maxW={["100%", "90%", "70%"]}
-              >
-                <Box bgColor="gray.200" p={1} borderRadius={20} paddingInline={4}
-                  cursor="pointer"
-                  _hover={{ shadow: "xl" }}
-                  transition="all 0.1s ease-in-out"
-                  onClick={() => navigate({
-                    pathname: pathname,
-                    search: `?courseID=${courseID}&week=1`,
-                  })}>
-                  <Text fontWeight="medium">1</Text>
-                </Box>
-                <Box bgColor="gray.200" p={1} borderRadius={20} paddingInline={4}
-                  cursor="pointer"
-                  _hover={{ shadow: "xl" }}
-                  transition="all 0.1s ease-in-out"
-                  onClick={() => navigate({
-                    pathname: pathname,
-                    search: `?courseID=${courseID}&week=2`,
-                  })}>
-                  <Text fontWeight="medium">2</Text>
-                </Box>
-                <Box bgColor="gray.200" p={1} borderRadius={20} paddingInline={4}
-                  cursor="pointer"
-                  _hover={{ shadow: "xl" }}
-                  transition="all 0.1s ease-in-out"
-                  onClick={() => navigate({
-                    pathname: pathname,
-                    search: `?courseID=${courseID}&week=3`,
-                  })}>
-                  <Text fontWeight="medium">3</Text>
-                </Box>
-                <Box bgColor="gray.200" p={1} borderRadius={20} paddingInline={4}
-                  cursor="pointer"
-                  _hover={{ shadow: "xl" }}
-                  transition="all 0.1s ease-in-out"
-                  onClick={() => navigate({
-                    pathname: pathname,
-                    search: `?courseID=${courseID}&week=4`,
-                  })}>
-                  <Text fontWeight="medium">4</Text>
-                </Box>
-                <Box bgColor="gray.200" p={1} borderRadius={20} paddingInline={4}
-                  cursor="pointer"
-                  _hover={{ shadow: "xl" }}
-                  transition="all 0.1s ease-in-out"
-                  onClick={() => navigate({
-                    pathname: pathname,
-                    search: `?courseID=${courseID}&week=5`,
-                  })}>
-                  <Text fontWeight="medium">5</Text>
-                </Box>
-                <Box bgColor="gray.200" p={1} borderRadius={20} paddingInline={4}
-                  cursor="pointer"
-                  _hover={{ shadow: "xl" }}
-                  transition="all 0.1s ease-in-out"
-                  onClick={() => navigate({
-                    pathname: pathname,
-                    search: `?courseID=${courseID}&week=6`,
-                  })}>
-                  <Text fontWeight="medium">6</Text>
-                </Box>
-                <Box bgColor="gray.200" p={1} borderRadius={20} paddingInline={4}
-                  cursor="pointer"
-                  _hover={{ shadow: "xl" }}
-                  transition="all 0.1s ease-in-out"
-                  onClick={() => navigate({
-                    pathname: pathname,
-                    search: `?courseID=${courseID}&week=7`,
-                  })}>
-                  <Text fontWeight="medium">7</Text>
-                </Box>
-                <Box bgColor="gray.200" p={1} borderRadius={20} paddingInline={4}
-                  cursor="pointer"
-                  _hover={{ shadow: "xl" }}
-                  transition="all 0.1s ease-in-out"
-                  onClick={() => navigate({
-                    pathname: pathname,
-                    search: `?courseID=${courseID}&week=8`,
-                  })}>
-                  <Text fontWeight="medium">8</Text>
-                </Box>
-                <Box bgColor="gray.200" p={1} borderRadius={20} paddingInline={4}
-                  cursor="pointer"
-                  _hover={{ shadow: "xl" }}
-                  transition="all 0.1s ease-in-out"
-                  onClick={() => navigate({
-                    pathname: pathname,
-                    search: `?courseID=${courseID}&week=9`,
-                  })}>
-                  <Text fontWeight="medium">9</Text>
-                </Box>
-                <Box bgColor="gray.200" p={1} borderRadius={20} paddingInline={4}
-                  cursor="pointer"
-                  _hover={{ shadow: "xl" }}
-                  transition="all 0.1s ease-in-out"
-                  onClick={() => navigate({
-                    pathname: pathname,
-                    search: `?courseID=${courseID}&week=10`,
-                  })}>
-                  <Text fontWeight="medium">10</Text>
-                </Box>
-                <Box bgColor="gray.200" p={1} borderRadius={20} paddingInline={4}
-                  cursor="pointer"
-                  _hover={{ shadow: "xl" }}
-                  transition="all 0.1s ease-in-out"
-                  onClick={() => navigate({
-                    pathname: pathname,
-                    search: `?courseID=${courseID}&week=11`,
-                  })}>
-                  <Text fontWeight="medium">11</Text>
-                </Box>
-                <Box bgColor="gray.200" p={1} borderRadius={20} paddingInline={4}
-                  cursor="pointer"
-                  _hover={{ shadow: "xl" }}
-                  transition="all 0.1s ease-in-out"
-                  onClick={() => navigate({
-                    pathname: pathname,
-                    search: `?courseID=${courseID}&week=12`,
-                  })}>
-                  <Text fontWeight="medium">12</Text>
-                </Box>
-              </Flex>
-          </VStack>
-      );
-    };
+            >
+                {weeks.map((week) => (
+                    <Box
+                        key={week}
+                        bgColor="gray.200"
+                        p={1}
+                        borderRadius={20}
+                        paddingInline={4}
+                        cursor="pointer"
+                        _hover={{ shadow: "xl" }}
+                        transition="all 0.1s ease-in-out"
+                        onClick={() => handleWeekClick(week)}
+                    >
+                        <Text fontWeight="medium">{week}</Text>
+                    </Box>
+                ))}
+            </Flex>
+        </VStack>
+    );
+};
 
 export default WeeksColumn;
