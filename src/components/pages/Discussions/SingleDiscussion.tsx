eecdc1d28172fd9cc6c6a0e5eb70b626de11980f
Rebecca Marshall <rebeccamarshall@uvic.ca>
2023-03-05 19:06:24 -0800

    add static Create Discussion page

--- src/components/pages/Discussions/SingleDiscussion.tsx
+++ src/components/pages/Discussions/SingleDiscussion.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack, Text} from "@chakra-ui/react";
+import { Box, VStack, Flex, Text, HStack } from "@chakra-ui/react";
 import { useSearchParams } from "react-router-dom";
 import CourseHeader from "../../common/CourseHeader";
 import WeeksColumn from "../../common/WeeksColumn";
@@ -9,13 +9,19 @@ const SingleDiscussion = () => {
     const courseID = id == null ? '': id; // edit if you know a more graceful way to handle the null
 
     return (
-      <Box>
-        <CourseHeader courseID={courseID} />
-        <Box w="100%" h={0.5} bgColor="white" />
-        <HStack>
-          <WeeksColumn courseID={courseID}/>
-          <Text> Single Discussion Page </Text>
-        </HStack>
+        <Box>
+            <CourseHeader courseID={courseID} />
+            <Box w="100%" h={0.5} bgColor="white" />
+            <Flex justifyContent="space-between">
+                <WeeksColumn courseID={courseID} />
+                <Flex w="100%" justifyContent="center" mt={6}>
+                    <VStack w="80%" maxW="1000px" mt={6}>
+                        <Text fontWeight="medium" fontSize="x-large" >Discussion</Text>
+                        <HStack w="100%">
+                        </HStack>  
+                    </VStack>
+                </Flex>
+            </Flex>
       </Box>
     );
   };
