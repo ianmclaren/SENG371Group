4b11d938cb19f2ca2c50f83961867bed7973d7f5
Stefan Esqivel <sesquivel@uvic.ca>
2023-03-05 21:18:22 -0800

    Added dummy tests for the page

---
+++ src/components/organisms/CourseDashboard.test.tsx
@@ -0,0 +1,27 @@
+import { render } from "@testing-library/react"
+import CourseDashboard from "./CourseDashboard"
+
+//
+describe(CourseDashboard, () => {
+    it("should allow user to click in to there course page", () => {
+        expect(true).toBe(true)
+    });
+});
+
+describe(CourseDashboard, () => {
+    it("should allow user to click in to there account page", () => {
+        expect(true).toBe(true)
+    });
+});
+
+describe(CourseDashboard, () => {
+    it("should allow user to see notifications", () => {
+        expect(true).toBe(true)
+    });
+});
+
+describe(CourseDashboard, () => {
+    it("should allow user to select current term", () => {
+        expect(true).toBe(true)
+    });
+});
