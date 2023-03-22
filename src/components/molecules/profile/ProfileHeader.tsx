681df05a438a1779cda020136ff63bb46941971d
Shake1999 <northstarsh@gmail.com>
2023-03-22 16:44:06 -0700

    profile page basics

---
+++ src/components/molecules/profile/ProfileHeader.tsx
@@ -0,0 +1,62 @@
+import { Box, Text, VStack, HStack, Button, Icon } from "@chakra-ui/react";
+import { DeleteIcon} from "@chakra-ui/icons";
+
+const ProfileHeader = () => {
+  const buttonStyle = {
+    bgColor: "gray.200",
+    _hover: { shadow: "xl" },
+    _dark: {
+      bgColor: "gray.600",
+      _hover: { shadow: "dark-lg" }
+    },
+  }
+
+  return (
+    <HStack p={2} margin={3} border="solid" borderRadius={10} borderWidth={1}>
+      <Box width="12%">
+        <VStack p={1}>
+          <img src="https://media.istockphoto.com/id/507995592/photo/pensive-man-looking-at-the-camera.jpg?s=170667a&w=0&k=20&c=gEjwbwM9D_0PERe2yR_b8dec_F1e6FrLf7yvWTdm4NY=" alt="profile" 
+            style={{ maxWidth: '100%', borderRadius: '7%', overflow: "hidden"}}/>
+          <HStack width="100%">
+            <Button {...buttonStyle} borderRadius={20} paddingInline={5}
+              cursor="pointer"
+              transition="all 0.1s ease-in-out">
+                <Text fontWeight="medium">Change Picture</Text>
+              </Button>
+            <Icon
+              as={DeleteIcon}
+              fontSize="xl"
+              _hover={{ color: "gray.500" }}
+            />
+          </HStack>
+        </VStack>
+      </Box>
+      <HStack w="100%" p={3} gap={100}>
+        <Box width="45%">
+          <Text fontWeight="medium" fontSize="xl" letterSpacing={3} textTransform="uppercase">John Smith</Text>
+          <Text fontWeight="light" fontSize="sm">V00123456</Text>
+        </Box>
+        <Box>
+            <Text fontWeight="medium" fontSize="xl" letterSpacing={3} textTransform="uppercase">BIO</Text>
+            <Text fontWeight="light" fontSize="sm">
+                Lorem ipsum dolor sit amet. Sed iusto molestiae est blanditiis dolore aut voluptatem beatae nam iure reprehenderit ut obcaecati rerum aut exercitationem consectetur. Id necessitatibus debitis hic explicabo reprehenderit nam praesentium.
+            </Text>
+            <HStack width="100%">
+                <Button {...buttonStyle} borderRadius={20} paddingInline={5}
+                    cursor="pointer"
+                    transition="all 0.1s ease-in-out">
+                    <Text fontWeight="medium">Edit Bio</Text>
+                </Button>
+            <Icon
+              as={DeleteIcon}
+              fontSize="xl"
+              _hover={{ color: "gray.500" }}
+            />
+          </HStack>
+        </Box>
+      </HStack>
+    </HStack>
+  );
+};
+
+export default ProfileHeader;
