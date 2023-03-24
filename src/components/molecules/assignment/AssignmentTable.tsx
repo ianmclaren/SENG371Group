d60aebb130eb53e41fa829a7b11e4b6dfe39ac5b
ianmclaren <ianmclaren@uvic.ca>
2023-03-23 22:09:34 -0700

    Removing unused comments and imports

--- src/components/molecules/assignment/AssignmentTable.tsx
+++ src/components/molecules/assignment/AssignmentTable.tsx
@@ -9,7 +9,6 @@ import {
     Td,
     Text,
 } from "@chakra-ui/react";
-import { useNavigate, useParams } from "react-router-dom";
 import { Assignment } from "../../../utils/types";
 
 const AssignmentTable = ({ assignments }: { assignments: Assignment[] }) => {
