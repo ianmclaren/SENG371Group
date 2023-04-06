b977c6c90a7c359fc3a87c0518831c73daed97bb
Eduardo Szeckir <eszeckirjr@gmail.com>
2023-04-05 22:31:16 -0700

    remove test file

--- src/components/common/DimspaceCalendar.tsx
+++ src/components/common/DimspaceCalendar.tsx
@@ -341,7 +341,6 @@ const DimspaceCalendar = ({ view }: { view: ViewType }) => {
         isOpen={isEventCreateOpen}
         onClose={onEventCreateClose}
       />
-      <App view="week" />
     </Box>
   );
 };
